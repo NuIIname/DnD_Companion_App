875112ce14a2481407cf124e3a39e1897c3c2bb3
Fulcanelli <slyman01202@gmail.com>
2026-04-11 17:39:25 -0500

    Test of git checkout

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'D&D Companion App',
+      title: 'This is a Test',
       theme: ThemeData(
         // This is the theme of your application.
         //
