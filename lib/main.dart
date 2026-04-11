29ab19d4a29c8454adabedb06295874f7d6f6a9f
Fulcanelli <slyman01202@gmail.com>
2026-04-11 17:54:47 -0500

    Branch test #2

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'This is a Test',
+      title: 'D&D Companion App',
       theme: ThemeData(
         // This is the theme of your application.
         //
